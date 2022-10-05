f90496d691818149098c43c260e9ee0e75ce88d5
lucianovilela <lucianovilela@gmail.com>
2022-10-05 14:52:37 +0000

    finalizado

--- pages/cartao.js
+++ pages/cartao.js
@@ -4,10 +4,12 @@ import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
 import IconButton from "@mui/material/IconButton";
+import CircularProgress from "@mui/material/CircularProgress";
 import Header from "../src/Header";
 import { useMyContext } from "../src/MyContext";
 import BadgeOutlinedIcon from '@mui/icons-material/BadgeOutlined';
 import DoNotDisturbOnIcon from '@mui/icons-material/DoNotDisturbOn';
+import { red, blue, green } from '@mui/material/colors';
 
 import { firestore } from "../src/db";
 import {
@@ -22,10 +24,9 @@ import { async } from "@firebase/util";
 export default function Index() {
   const [{ user }] = useMyContext();
 
-
   const [list, setList] = React.useState([]);
-  React.useEffect(() => {
 
+  const loadList = () => {
     const docRef = collection(firestore, "user");
 
     getDocsFromServer(docRef).then((docs) => {
@@ -37,14 +38,19 @@ export default function Index() {
       });
       setList(l);
     });
+  }
+
+  React.useEffect(() => {
+    loadList();
+
   }, []);
 
-  const trocaCartao= async(id)=>{
-    
-    const docRef = doc(firestore,"user", id );
+  const trocaCartao = async ({ id, cartao }) => {
+
+    const docRef = doc(firestore, "user", id);
     console.log(docRef.document);
-    await updateDoc(docRef, {cartao:true})
-  
+    await updateDoc(docRef, { cartao: !cartao })
+    loadList();
 
   }
 
@@ -53,24 +59,28 @@ export default function Index() {
       <Header />
 
       <Container maxWidth="sm">
-        <Typography variant="h4">Distribuição de cartões </Typography>
-        <Box sx={{ my: 4 }}>
-          {list.map((item) => (
-            <Stack direction="row" spacing={2}  alignContent={'center'}>
-              <Typography>{item.nome} </Typography>
-              <IconButton variant="outlined" onClick={()=>trocaCartao(item.id)}>
-                {item.cartao ?
-                  <BadgeOutlinedIcon />
-                  : <DoNotDisturbOnIcon />
-                }
-              </IconButton >
+        <Typography variant="primary" >Distribuição de cartões </Typography>
+        <br/>
+        {list.length!==0 ?
+
+          <Box sx={{ my: 4 }}>
+            {list.map((item) => (
+              <Stack direction="row" spacing={2} alignItems={'center'} justifyContent={'left'}>
+                <Typography>{item.nome} </Typography>
+                <IconButton variant="outlined" onClick={() => trocaCartao(item)}>
+                  {item.cartao ?
+                    <BadgeOutlinedIcon sx={{ color: blue[500] }}/>
+                    : <DoNotDisturbOnIcon sx={{ color: red[500] }}/>
+                  }
+                </IconButton >
 
 
 
-            </Stack>
-          ))}
-        </Box>
-
+              </Stack>
+            ))}
+          </Box>
+          : <CircularProgress />
+        }
       </Container>
     </>
   );
